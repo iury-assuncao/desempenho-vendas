fbe4e11ffea120a2c10b842a7dc24f44db0af8dd
Iury Assunção <iuryifba@gmail.com>
2024-09-18 18:12:09 -0300

    feat: adiciona a quantidade de funcionários que atingiram a meta

--- index.r
+++ index.r
@@ -3,4 +3,7 @@ vendasFuncionarios<-c(5,30,14,22,27,16,26,8,19,34)
 
 vendedores<-c("Joana","Bianca", "Arthur", "José", "Yan", "Gustavo", "Jorge", "Lanna", "Helena", "Evilly")
 
+atingiuMeta <- vendasFuncionarios >= 20
+qtdMetaAtingida <- sum(atingiuMeta)
 
+cat("Total de vendedores que atingiram a meta: " ,qtdMetaAtingida)
